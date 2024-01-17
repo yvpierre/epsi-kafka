c8d1abcec08e32c82c16083b43c87722a77455fc
Pierre <pierre.yvenou@orange.fr>
2024-01-17 15:38:33 +0100

    Erreur serveur kafka

--- src/cancelled_flights/producer.js
+++ src/cancelled_flights/producer.js
@@ -1,18 +1,19 @@
 const { Kafka } = require('kafkajs')
 
 const kafka = new Kafka({
-    clientId: 'my-app',
-    brokers: ['kafka1:9092', 'kafka2:9092'],
+    clientId: 'cancelled-flights-producer',
+    brokers: ['kafka1:9092'],
 })
 
 const producer = kafka.producer()
-
 await producer.connect()
+
 await producer.send({
     topic: 'test-topic',
     messages: [
         { value: 'Hello KafkaJS user!' },
     ],
+    setTimeout: 1000,
 })
 
 await producer.disconnect()
--- src/index.js
+++ src/index.js
@@ -14,3 +14,4 @@ app.get('/', (req, res) => {
 app.listen(port, () => {
     console.log(`[server]: Server is running at http://localhost:${port}`);
 });
+
