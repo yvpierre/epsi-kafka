5cdc6edd326248f8ca1dac479754909cd75fdf3f
simonhuchede <simon.huchede@7opteam.com>
2024-01-23 16:04:52 +0100

    feat: format data from klm api to send message about companies status count

--- src/cancelled_flights/consumer.js
+++ src/cancelled_flights/consumer.js
@@ -1,17 +1,19 @@
 const {Kafka} = require("kafkajs");
 const kafka = new Kafka({
-    clientId: 'cancelled-flights-producer',
-    brokers: ['kafka1:9092'],
+    clientId: 'cancelled-flights-consumer',
+    brokers: ['localhost:9092'],
 })
 const consumer = kafka.consumer({ groupId: 'test-group' })
+async function main(){
+    await consumer.connect()
+    await consumer.subscribe({ topic: 'airlines-status-count-topic', fromBeginning: true })
 
-await consumer.connect()
-await consumer.subscribe({ topic: 'test-topic', fromBeginning: true })
-
-await consumer.run({
-    eachMessage: async ({ topic, partition, message }) => {
-        console.log({
-            value: message.value.toString(),
-        })
-    },
-})
+    await consumer.run({
+        eachMessage: async ({ topic, partition, message }) => {
+            console.log({
+                value: message.value.toString(),
+            })
+        },
+    })
+}
+main();
--- src/cancelled_flights/producer.js
+++ src/cancelled_flights/producer.js
@@ -1,21 +1,98 @@
-const { Kafka } = require('kafkajs')
+const { Kafka } = require('kafkajs');
+const axios = require('axios');
+
 
 const kafka = new Kafka({
     clientId: 'cancelled-flights-producer',
     brokers: ['localhost:9092'],
 })
-async function getMessage(){
-    const producer = kafka.producer()
-    await producer.connect()
 
-    await producer.send({
-        topic: 'test-topic',
-        messages: [
-            { value: 'Hello KafkaJS user!' },
-        ],
-        setTimeout: 1000,
-    })
-
-    await producer.disconnect()
+const apiKey = 'av59smnptjuqw4b8upayeaau';
+const apiUrl = 'https://api.airfranceklm.com/opendata/flightstatus/';
+const requestOptions = {
+    method: 'GET',
+    headers: {
+        'Content-Type': 'application/json',
+        'API-Key': apiKey,
+    }
+};
+async function getKlmResponse(){
+    try {
+        const response = await axios.get(apiUrl, requestOptions);
+        return response.data.operationalFlights;
+    } catch (error) {
+        console.error('Error fetching data:', error);
+        throw error;
+    }
 }
-getMessage();
+
+async function fetchDataAndLog() {
+    try {
+        return await getKlmResponse();
+    } catch (error) {
+        console.error('Error fetching and logging data:', error);
+    }
+}
+
+async function sendMessage(messages){
+const producer = kafka.producer()
+await producer.connect()
+await producer.send({
+    topic: 'airlines-status-count-topic',
+    messages: messages
+    ,
+    setTimeout: 1000,
+})
+
+await producer.disconnect()
+}
+
+async function run() {
+    try {
+        const json = await fetchDataAndLog();
+        const dictionnaireResultat = createFlightStatusDictionary(json);
+        const statusList = ['ON_TIME', 'CANCELLED', 'DELAYED_DEPARTURE'];
+        const statusCount = {};
+        for (const airline in dictionnaireResultat) {
+            const statuses = dictionnaireResultat[airline];
+            const infoList = statusList.map(status => {
+                const count = statuses.filter(s => s === status).length;
+                return `${status}: ${count}`;
+            });
+            statusCount[airline] = infoList;
+        }
+
+        let messages =  [];
+        for(let airline in statusCount){
+            const message = {
+                key: 'statusCountByAirlineCompany',
+                value: `${airline}: ${JSON.stringify(statusCount[airline])}`,
+            };
+            messages.push(message);
+        }
+
+        await sendMessage(messages);
+        return "ok";
+    } catch (error) {
+        console.error('Error:', error);
+        return { error: 'An error occurred.' };
+    }
+}
+
+function createFlightStatusDictionary(json) {
+        let dictionnaire = {};
+        json.forEach(flight => {
+            var nomCompagnie = flight.airline.name;
+            var statutVol = flight.flightStatusPublic;
+            if (dictionnaire[nomCompagnie] === undefined) {
+                dictionnaire[nomCompagnie] = [statutVol];
+            } else {
+                dictionnaire[nomCompagnie].push(statutVol);
+            }
+        });
+
+        return dictionnaire;
+}
+const interval = 20 * 1000;
+
+setInterval(run, interval);
